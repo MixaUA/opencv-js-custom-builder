f282c12c874cc9a440b6309c900c4e3cd57c634a
MixaUA <121048487+MixaUA@users.noreply.github.com>
2026-07-19 21:51:03 +0300

    Update minimal_config.py

--- minimal_config.py
+++ minimal_config.py
@@ -1,35 +1,68 @@
 # minimal_config.py
-# Хірургічний мінімум функцій OpenCV для роботи нашого круглокоду
-#
-# Формат конфіга opencv.js: окремі словники по модулях, ключ ''
-# означає "вільні функції модуля" (не методи класу). makeWhiteList()
-# вже є всередині генератора байндингів OpenCV - визначати самим не треба.
-#
-# Класи Mat/Rect/Point/Size/Scalar/RotatedRect та константи
-# (COLOR_BGR2GRAY, THRESH_BINARY, RETR_TREE, CHAIN_APPROX_SIMPLE...)
-# підключаються автоматично разом із функціями, які їх використовують.
-#
-# imread/imwrite прибрані: в браузері немає файлової системи.
-# Замість них - imencode/imdecode (буфер у памʼяті).
+# Мінімальний набір OpenCV.js для розпізнавання круглокоду
 
 core = {
     '': [
-        'countNonZero', 'minMaxLoc', 'bitwise_xor', 'bitwise_and', 'bitwise_not',
-        'transpose', 'flip'
+        'countNonZero',
+        'minMaxLoc',
+
+        'bitwise_xor',
+        'bitwise_and',
+        'bitwise_not',
+
+        'transpose',
+        'flip'
     ]
 }
 
 imgproc = {
     '': [
-        'cvtColor', 'threshold', 'adaptiveThreshold', 'GaussianBlur', 'medianBlur',
-        'findContours', 'contourArea', 'arcLength', 'approxPolyDP',
-        'moments', 'minEnclosingCircle', 'fitEllipse', 'boundingRect',
-        'warpAffine', 'getRotationMatrix2D', 'warpPerspective', 'getPerspectiveTransform'
+        # Перетворення та фільтрація
+        'cvtColor',
+        'threshold',
+        'adaptiveThreshold',
+        'GaussianBlur',
+        'medianBlur',
+
+        # Морфологія
+        'getStructuringElement',
+        'erode',
+        'dilate',
+        'morphologyEx',
+
+        # Контури
+        'findContours',
+        'contourArea',
+        'arcLength',
+        'approxPolyDP',
+        'convexHull',
+        'isContourConvex',
+
+        # Геометрія
+        'moments',
+        'boundingRect',
+        'minAreaRect',
+        'boxPoints',
+        'minEnclosingCircle',
+        'fitEllipse',
+
+        # Перетворення
+        'getRotationMatrix2D',
+        'warpAffine',
+        'getPerspectiveTransform',
+        'warpPerspective'
     ]
 }
 
 imgcodecs = {
-    '': ['imencode', 'imdecode']
+    '': [
+        'imencode',
+        'imdecode'
+    ]
 }
 
-white_list = makeWhiteList([core, imgproc, imgcodecs])
+white_list = makeWhiteList([
+    core,
+    imgproc,
+    imgcodecs
+])
