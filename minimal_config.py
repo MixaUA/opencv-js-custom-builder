557af9a2332d97e72782f48e25b42a13aa12334b
MixaUA <121048487+MixaUA@users.noreply.github.com>
2026-07-19 17:21:45 +0300

    Create minimal_config.py

---
+++ minimal_config.py
@@ -0,0 +1,19 @@
+# minimal_config.py
+# Визначаємо мінімальний набір функцій для нашого енкодера/декодера
+
+white_list = {
+    'core': [
+        'Mat', 'Rect', 'Point', 'Size', 'Scalar',
+        'countNonZero', 'minMaxLoc', 'bitwise_xor', 'bitwise_and', 'bitwise_not'
+    ],
+    'imgproc': [
+        'cvtColor', 'threshold', 'adaptiveThreshold', 'GaussianBlur', 'medianBlur',
+        'findContours', 'contourArea', 'arcLength', 'approxPolyDP',
+        'warpAffine', 'getRotationMatrix2D', 'warpPerspective', 'getPerspectiveTransform',
+        'COLOR_BGR2GRAY', 'COLOR_RGBA2GRAY', 'THRESH_BINARY', 'THRESH_BINARY_INV', 
+        'THRESH_OTSU', 'ADAPTIVE_THRESH_GAUSSIAN_C', 'RETR_EXTERNAL', 'CHAIN_APPROX_SIMPLE'
+    ],
+    'imgcodecs': [
+        'imread', 'imwrite', 'imencode', 'imdecode'
+    ]
+}
